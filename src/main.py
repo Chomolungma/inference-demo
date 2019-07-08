7c4199036e2ab2924d6087832497a19b0b5c5fe4
eramirez392 <enrique.ramirez@ridgerun.com>
2019-07-08 17:06:39 -0600

    Implement RTSP + GstInterpipe + GstWebRTC streaming

--- src/main.py
+++ src/main.py
@@ -4,13 +4,18 @@ import logging
 import gi
 import sys
 import time
+import json
 gi.require_version('Gst', '1.0')
 gi.require_version('GObject', '2.0')
 from gi.repository import GObject, Gst, GLib
 from gst import pygstd
 
-pipeline1 = "videotestsrc pattern=ball is-live=true ! capsfilter caps=video/x-raw,width=640,height=480 ! xvimagesink"
-pipeline2 = "videotestsrc is-live=true ! xvimagesink"
+webrtc_base_pipeline = " rrwebrtcbin rtcp-mux=true start-call=true "
+rstp_source_pipeline = " rtspsrc debug=true async-handling=true location=rtsp://"
+video_decode_pipeline = " rtpvp8depay ! omxvp8dec ! nvvidconv ! capsfilter caps=video/x-raw(memory:NVMM) ! nvvidconv "
+interpipesink_pipeline = " interpipesink enable-last-sample=false forward-eos=true forward-events=true async=false name="
+interpipesrc_pipeline = " interpipesrc format=3 listen-to="
+video_encode_pipeline = " queue max-size-buffers=1 leaky=downstream ! omxvp8enc ! rtpvp8pay"
 
 pipeline_counter = 0
 
@@ -35,19 +40,48 @@ def play_pipeline(gstd_client, pipeline):
         return
 
     pipeline_counter = pipeline_counter + 1
-    
+
+def build_test_0(gstd_client, test_name, default_data):
+    user_channel = default_data[test_name]["user-channel"]
+    peer_channel = default_data[test_name]["peer-channel"]
+    rtsp_ip_address = default_data[test_name]["rtsp_ip_address"]
+    rtsp_port = default_data[test_name]["rtsp_port"]
+
+    # Create Pipelines
+    webrtc_name = test_name + ".video_sink"
+    webrtc = webrtc_base_pipeline + "signaler::user-channel=" + user_channel
+    webrtc += " signaler::peer-channel=" + peer_channel
+    webrtc += " name=" + test_name
+
+    rtsp = rstp_source_pipeline + rtsp_ip_address + ":" + rtsp_port + "/test"
+
+    interpipesink0_name = test_name + "_decodesink"
+
+    video_receive = video_decode_pipeline + " ! "
+    video_receive += interpipesink_pipeline + interpipesink0_name
+
+    video_send = interpipesrc_pipeline + interpipesink0_name
+    video_send += " ! " + video_encode_pipeline
+
+    full_pipe = webrtc + "  " + rtsp + " ! " + video_receive + video_send + " ! " + webrtc_name
+    logging.info(" Test name: " + test_name)
+    logging.info(" Description: RTSP + GstInterpipe + GstWebRTC on GStreamer Daemon")
+    logging.info(" Pipeline: " + full_pipe)
+    play_pipeline(gstd_client, full_pipe)
+
 def main (args=None):
     gstd_client = pygstd.GSTD()
+
+    # Load the JSON default parameters as a dictionary
+    with open('./pipe_config.json') as json_file:
+        default_params = json.load(json_file)
 
     # Logger Setup
     logger_setup()
 
     logging.info("This is a demo application...")
     loop = GObject.MainLoop()
-    logging.info(" Pipeline: " + pipeline1)
-    play_pipeline(gstd_client, pipeline1)
-    logging.info(" Pipeline: " + pipeline2)
-    play_pipeline(gstd_client, pipeline2)
+    build_test_0(gstd_client, "Test0", default_params)
 
     try:
         loop.run()
