31444f98a2c6f1e79e8a13530703f8d626eb173e
Emmanuel Madrigal <emmanuel.madrigal@ridgerun.com>
2019-07-15 09:33:14 -0600

    Apply pep8 format

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from gst import gstc
+from gi.repository import GObject, Gst, GLib
 import logging
 import gi
 import sys
@@ -7,11 +9,9 @@ import time
 import json
 gi.require_version('Gst', '1.0')
 gi.require_version('GObject', '2.0')
-from gi.repository import GObject, Gst, GLib
-from gst import gstc
 
 # Read Tinyyolo Labels
-tinyyolo_labels_file = open("tinyyolov2_labels.txt","r")
+tinyyolo_labels_file = open("tinyyolov2_labels.txt", "r")
 tinyyolo_labels = tinyyolo_labels_file.read()
 tinyyolo_labels_file.close()
 
@@ -31,44 +31,53 @@ tee_pipeline = " tee name=t"
 
 # Inference (Tinyyolov2)
 tinyyolov2_format_pipeline = " capsfilter caps=video/x-raw,width=752,height=480 "
-tinyyolov2_base_pipeline = """ tinyyolov2 model-location="""+models_path+"""graph_tinyyolov2_tensorflow.pb backend=tensorflow backend::input-layer=input/Placeholder backend::output-layer=add_8 name=net """
+tinyyolov2_base_pipeline = """ tinyyolov2 model-location=""" + models_path + \
+    """graph_tinyyolov2_tensorflow.pb backend=tensorflow backend::input-layer=input/Placeholder backend::output-layer=add_8 name=net """
 tinyyolov2_net_pipeline = " t. ! queue max-size-buffers=1 leaky=downstream ! nvvidconv ! capsfilter caps=video/x-raw(memory:NVMM) ! nvvidconv ! net.sink_model "
 tinyyolov2_bypass_pipeline = " t. ! queue max-size-buffers=1 leaky=downstream ! net.sink_bypass "
-tinyyolov2_overlay_pipeline = """ net.src_bypass ! nvvidconv ! capsfilter caps=video/x-raw(memory:NVMM) ! nvvidconv ! detectionoverlay labels=\""""+tinyyolo_labels+"""\" ! inferencealert name=person-alert label-index=14 ! queue max-size-buffers=1 leaky=downstream ! nvvidconv ! capsfilter caps=video/x-raw(memory:NVMM)  ! nvvidconv ! capsfilter caps=video/x-raw """
+tinyyolov2_overlay_pipeline = """ net.src_bypass ! nvvidconv ! capsfilter caps=video/x-raw(memory:NVMM) ! nvvidconv ! detectionoverlay labels=\"""" + tinyyolo_labels + \
+    """\" ! inferencealert name=person-alert label-index=14 ! queue max-size-buffers=1 leaky=downstream ! nvvidconv ! capsfilter caps=video/x-raw(memory:NVMM)  ! nvvidconv ! capsfilter caps=video/x-raw """
+
 
 def logger_setup():
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     root.addHandler(handler)
 
+
 def create_pipeline(gstd_client, name, pipeline):
     global pipeline_counter
-    ret = gstd_client.pipeline_create (name, pipeline)
-    if (ret!=0):
-        print ("Error creating the pipeline: "+ str(ret))
+    ret = gstd_client.pipeline_create(name, pipeline)
+    if (ret != 0):
+        print("Error creating the pipeline: " + str(ret))
         return
 
+
 def play_pipeline(gstd_client, name):
-    ret = gstd_client.pipeline_play (name)
-    if (ret!=0):
-        print ("Error playing the pipeline: "+ str(ret))
+    ret = gstd_client.pipeline_play(name)
+    if (ret != 0):
+        print("Error playing the pipeline: " + str(ret))
         return
+
 
 def set_element_prop(gstd_client, name, element, prop, value):
     ret = gstd_client.element_set(name, element, prop, value)
-    if (ret!=0):
-        print ("Error setting element property: "+ str(ret))
+    if (ret != 0):
+        print("Error setting element property: " + str(ret))
         return
 
+
 def stop_pipeline(gstd_client, name):
-    ret = gstd_client.pipeline_stop (name)
-    if (ret!=0):
-        print ("Error stopping the pipeline: "+ str(ret))
+    ret = gstd_client.pipeline_stop(name)
+    if (ret != 0):
+        print("Error stopping the pipeline: " + str(ret))
         return
+
 
 def build_test_0(gstd_client, test_name, default_data):
     session_id = default_data[test_name]["session_id"]
@@ -87,7 +96,8 @@ def build_test_0(gstd_client, test_name, default_data):
 
     video_receive0 = interpipesink_pipeline + interpipesink0_name
 
-    video_receive1 = video_decode_pipeline + " ! " + tinyyolov2_format_pipeline + " ! "
+    video_receive1 = video_decode_pipeline + \
+        " ! " + tinyyolov2_format_pipeline + " ! "
     video_receive1 += interpipesink_pipeline + interpipesink1_name
 
     inference = tinyyolov2_base_pipeline
@@ -98,15 +108,18 @@ def build_test_0(gstd_client, test_name, default_data):
 
     video_send = inference + " ! " + video_encode_pipeline
 
-    full_pipe = webrtc + "  " + camera_source_pipeline + " ! " + video_receive0 +  rtsp + " ! " + video_receive1 + video_send + " ! " + webrtc_name
+    full_pipe = webrtc + "  " + camera_source_pipeline + " ! " + video_receive0 + \
+        rtsp + " ! " + video_receive1 + video_send + " ! " + webrtc_name
 
     logging.info(" Test name: " + test_name)
-    logging.info(" Description: RTSP + GstInterpipe + GstInference Detection + GstWebRTC on GStreamer Daemon")
+    logging.info(
+        " Description: RTSP + GstInterpipe + GstInference Detection + GstWebRTC on GStreamer Daemon")
     logging.info(" Pipeline: " + full_pipe)
     create_pipeline(gstd_client, "p0", full_pipe)
     play_pipeline(gstd_client, "p0")
 
-def main (args=None):
+
+def main(args=None):
     gstd_client = gstc.client(loglevel='DEBUG')
 
     # Load the JSON default parameters as a dictionary
@@ -121,20 +134,32 @@ def main (args=None):
 
     time.sleep(1)
     while True:
-        choice = input("    ** Menu **\n 1) Camera source\n 2) RTSP source\n 3) Exit\n > ")
-        choice = choice.lower() #Convert input to "lowercase"
+        choice = input(
+            "    ** Menu **\n 1) Camera source\n 2) RTSP source\n 3) Exit\n > ")
+        choice = choice.lower()  # Convert input to "lowercase"
 
         if choice == '1':
-            set_element_prop(gstd_client, "p0", "src", "listen-to", "Test0_camera")
+            set_element_prop(
+                gstd_client,
+                "p0",
+                "src",
+                "listen-to",
+                "Test0_camera")
             print("--> Camera source selected\n")
         if choice == '2':
-            set_element_prop(gstd_client, "p0", "src", "listen-to", "Test0_decodesink")
+            set_element_prop(
+                gstd_client,
+                "p0",
+                "src",
+                "listen-to",
+                "Test0_decodesink")
             print("--> RTSP source selected\n")
         if choice == '3':
             print("--> Exit\n")
             break
 
     stop_pipeline(gstd_client, "p0")
+
 
 if __name__ == "__main__":
     main(None)
